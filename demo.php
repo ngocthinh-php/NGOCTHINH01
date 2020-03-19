bd6680643dcf13275988925aac204a9f9d9cd6c8
ngocthinh-php <62285771+ngocthinh-php@users.noreply.github.com>
2020-03-19 17:21:17 +0700

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,15 @@
+<!doctype.html>
+<html>
+<head>
+	<title>DEMO</title>
+</head>
+<body>
+<form action="connect.php" method="POST">
+  <label for="fname">Ho Ten:</label><br>
+  <input type="text" id="name" name="name"><br>
+  <label for="lname">Email:</label><br>
+  <input type="text" id="lname" name="lname"><br><br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
