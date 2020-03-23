9203eb096a0c3e0ef0c51723bccb2ed84a0e555e
ngocthinh-php <62285771+ngocthinh-php@users.noreply.github.com>
2020-03-23 20:33:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 
 <h2>HTML Forms</h2>
 
-<form action="/action_page.php">
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+<form action="/action_page.php" method="get">
+  <label for="name">Ho Ten:</label><br>
+  <input type="text" id="name" name="name" ><br>
+  <label for="mail">Email:</label><br>
+  <input type="text" id="mail" name="mail"><br><br>
   <input type="submit" value="Submit">
 </form> 
 
