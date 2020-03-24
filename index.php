9c425ae23b984e398b29274c68c4b18b9984aefb
ngocthinh-php <62285771+ngocthinh-php@users.noreply.github.com>
2020-03-24 15:34:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,10 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<h2>HTML Forms</h2>
-
-<form action="/action_page.php" method="get">
-  <label for="name">Ho Ten:</label><br>
-  <input type="text" id="name" name="name" ><br>
-  <label for="mail">Email:</label><br>
-  <input type="text" id="mail" name="mail"><br><br>
-  <input type="submit" value="Submit">
+<form action="/form.php" method="POST">
+  <p> Ho Ten: <input type="text" name="fname" value=""></p>
+  <p> Email: <input type="text" namme="mail" value=""></p>
+  <input type="submit" name="submit" value="submit">
 </form> 
-
-<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
-
 </body>
 </html>
