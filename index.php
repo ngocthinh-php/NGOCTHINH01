6b00b17af66e08d803876e1646952fcdfa0d6c88
ngocthinh-php <62285771+ngocthinh-php@users.noreply.github.com>
2020-03-24 15:49:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <body>
 <form action="/form.php" method="POST">
   <p> Ho Ten: <input type="text" name="fname" value=""></p>
-  <p> Email: <input type="text" namme="mail" value=""></p>
+  <p> Email: <input type="text" name="mail" value=""></p>
   <input type="submit" name="submit" value="submit">
 </form> 
 </body>
